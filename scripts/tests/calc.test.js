0fd1f04fac12940e8de816fc4b4e0b2b1c154291
Cass Beckford <CassJBeckford282@outlook.com>
2024-09-28 13:56:40 +0000

    .

--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -1,9 +1,13 @@
-const addition = require(".../calc");
-const { TestScheduler } = require("jest");
+const addition = require("../calc");
 
 describe("Calculator", () => {
-    describe("Addition function", () => {
-        TestScheduler()
+    describe("addition function", () => {
+        test("should return 42 for 20 + 22", () => {
+            expect(addition(20, 22)).toBe(42);
+        });
+        test("should return 43 for 20 + 23", () => {
+            expect(addition(20, 23)).toBe(43);
+        });
     });
     describe("Subtraction function", () => {
 
